5d1ef016ea132473bec3d5ca26f0cf8589044f64
xiaoxuelong <85219634+xiaoxuelong@users.noreply.github.com>
2021-06-05 00:44:36 +0800

    Update scdzs.js

--- scdzs.js
+++ scdzs.js
@@ -1,7 +1,10 @@
+var body = $response.body;
+var obj = JSON.parse(body);
 
-let obj = JSON.parse($response.body);
 obj.data.allowItem = {"paperAnserNum":9999,"allowVideoNum":9999,"allowAllStardAnser":9999,"allowVideoAll":9999,"allowViewQuestionNum":9999,"order":2};
 obj.data.isVip = 1;
 obj.data.vipEndTime = 2841763419;
 obj.data.isPrint = 0;
-$done({body: JSON.stringify(obj)});
+
+body = JSON.stringify(obj);
+$done({body});
